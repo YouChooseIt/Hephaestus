134ac8add6493865d618c842edbc332dea270f32
wsndow0 <wsndow0@nathatpas.tel>
2024-10-26 09:51:42 +0100

    Update GrammarModule.java

--- src/main/java/io/github/qe7/features/impl/modules/impl/misc/GrammarModule.java
+++ src/main/java/io/github/qe7/features/impl/modules/impl/misc/GrammarModule.java
@@ -18,15 +18,17 @@ public class GrammarModule extends Module {
     public GrammarModule() {
         super("Grammar", "Fixing grammar of messages, also un-abbreviates abbreviations", ModuleCategory.MISC);
 
+        abbreviations.put("wassup", "What's up");
+        abbreviations.put("sup", "What's up");
         abbreviations.put("u", "you");
         abbreviations.put("ur", "your");
+        abbreviations.put("ure", "you are");
         abbreviations.put("r", "are");
         abbreviations.put("y", "why");
         abbreviations.put("l8r", "later");
         abbreviations.put("cya", "see you");
         abbreviations.put("ttyl", "talk to you later");
         abbreviations.put("brb", "be right back");
-        abbreviations.put("btw", "by the way");
         abbreviations.put("idk", "I don't know");
         abbreviations.put("lol", "laugh out loud");
         abbreviations.put("rofl", "rolling on the floor laughing");
@@ -44,7 +46,7 @@ public class GrammarModule extends Module {
         abbreviations.put("nvm", "never mind");
         abbreviations.put("tbh", "to be honest");
         abbreviations.put("ikr", "I know, right?");
-        abbreviations.put("wyd", "what you doing?");
+        abbreviations.put("wyd", "what are you doing?");
         abbreviations.put("hmu", "hit me up");
         abbreviations.put("bff", "best friends forever");
         abbreviations.put("asap", "as soon as possible");
@@ -54,16 +56,37 @@ public class GrammarModule extends Module {
         abbreviations.put("btw", "by the way");
         abbreviations.put("gr8", "great");
         abbreviations.put("plz", "please");
+        abbreviations.put("pls", "please");
         abbreviations.put("wth", "what the heck");
         abbreviations.put("wtf", "what the fuck");
         abbreviations.put("xoxo", "hugs and kisses");
         abbreviations.put("glhf", "good luck, have fun");
         abbreviations.put("gg", "good game");
         abbreviations.put("wp", "well played");
+        abbreviations.put("ggwp", "good game, well played");
+        abbreviations.put("ez", "easy");
         abbreviations.put("afaik", "as far as I know");
         abbreviations.put("omw", "on my way");
+        abbreviations.put("m8", "mate");
         abbreviations.put("GH", "GameHerobrine");
+        abbreviations.put("gh", "GameHerobrine");
         abbreviations.put("Cliff", "Shliff");
+        abbreviations.put("wsndow0", "skidder");
+        abbreviations.put("color", "colour");
+        abbreviations.put("colorful", "colourful");
+        abbreviations.put("armor", "armour");
+        abbreviations.put("favorite", "favourite");
+        abbreviations.put("neighbor", "neighbour");
+        abbreviations.put("Heshaestus", "Hephaestus");
+        abbreviations.put("fabrick", "fabric");
+        abbreviations.put("reliq", "relique");
+        abbreviations.put("da", "the");
+        abbreviations.put("wat", "what");
+        abbreviations.put("wut", "what");
+        abbreviations.put("fabric", "mcp");
+        abbreviations.put("license", "licence");
+        abbreviations.put("woooow_0", "nebuladreamer4");
+        abbreviations.put("nig", "nigga"); //N-Word pass by wsndow0
     }
 
     @Subscribe
@@ -81,6 +104,8 @@ public class GrammarModule extends Module {
                 message += ".";
             }
 
+            message = message.replaceFirst(message.charAt(0) + "", Character.toUpperCase(message.charAt(0)) + "");
+            //chat bugs osiris moment message.substring(0, 100); :fire: @shae
             chatPacket.message = message;
         }
     });
